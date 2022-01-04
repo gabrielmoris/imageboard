bfd6d806d1b3a430553f9efc746a4bb095c6af00
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-01-04 15:03:48 +0100

    added aws s3 cloud and upload images

--- public/js/app.js
+++ public/js/app.js
@@ -4,9 +4,12 @@ Vue.createApp({
     data() {
         return {
             name: "Imageboard",
-            seen: "",
             images: [],
-            imgClicked: "",
+            title: "",
+            description: "",
+            username: "",
+            file: null,
+            success: false,
         };
     },
     mounted() {
@@ -16,5 +19,31 @@ Vue.createApp({
                 console.log(data);
                 this.images = data;
             });
+    },
+    methods: {
+        clickHandler: function () {
+            // console.log("this: ", this);
+            const fd = new FormData();
+            fd.append("title", this.title);
+            fd.append("description", this.description);
+            fd.append("username", this.username);
+            fd.append("file", this.file);
+            fetch("/upload", {
+                method: "POST",
+                body: fd,
+            })
+                .then((res) => res.json())
+                .then((res) => {
+                    console.log("res: ", res);
+                    this.success = true;
+                })
+                .catch((err) => {
+                    console.log("Error uploading new image: ", err);
+                });
+        },
+        fileSelectHandler: function (e) {
+            // console.log("fileselected", e);
+            this.file = e.target.files[0];
+        },
     },
 }).mount("#main");
--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 const express = require("express");
 const app = express();
 const db = require("./db.js");
+const { uploader } = require("./upload.js");
+const s3= require("./s3")
 
 app.use(express.static("./public"));
-
 app.use(express.json());
+
+app.post("/upload", uploader.single("file"), s3.upload, (req, res) => {
+    console.log("req.body", req.body, "req.file", req.file);
+    if(req.file){
+        res.json({success: true})
+    }else{
+        res.json({success: false})
+    }
+});
 
 app.get("/get-img-info", (req, res) => {
     db.getImages()
         .then((allImg) => {
             res.json(allImg.rows);
         })
-        .catch((e) => {
-            console.log("error in db ", e);
+        .catch((err) => {
+            console.log("error in db ", err);
         });
 });
 
